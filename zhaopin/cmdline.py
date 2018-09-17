a45da019da92f9e0b1fe7ad36195bf1637179dd9
CooperMin <1013838002@qq.com>
2018-09-17 15:54:27 +0800

    项目文件

---
+++ zhaopin/cmdline.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+from scrapy import cmdline
+
+
+
+# cmdline.execute(['scrapy','crawl','lgo','-s','LOG_FILE= scrapy.log'])
+cmdline.execute(['scrapy','crawl','lgo'])
