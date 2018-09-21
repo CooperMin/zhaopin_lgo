4f87d4706ec3a667f6c04f79b7ae8b3265ea27c8
CooperMin <40023982+CooperMin@users.noreply.github.com>
2018-09-21 22:12:02 +0800

    Update lgo.py

--- zhaopin/spiders/lgo.py
+++ zhaopin/spiders/lgo.py
@@ -60,7 +60,6 @@ class LgoSpider(scrapy.Spider):
                 'Accept': 'application/json, text/javascript, */*; q=0.01',
                 'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
                 'Referer': referer,
-                'Origin':'https://www.lagou.com',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'X-Requested-With': 'XMLHttpRequest',
                 'X-Anit-Forge-Token': None,
